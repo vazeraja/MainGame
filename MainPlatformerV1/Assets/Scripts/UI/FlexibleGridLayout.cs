0a56d8ec37d0799b9711f8aea423b9bcec532427
vazeraja <68131084+vazeraja@users.noreply.github.com>
2021-06-10 23:15:24 -0400

    clean up

---
+++ MainPlatformerV1/Assets/Scripts/UI/FlexibleGridLayout.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace MainGame {
+    public class FlexibleGridLayout : LayoutGroup {
+        
+        
+        
+        public override void CalculateLayoutInputVertical() {
+            throw new System.NotImplementedException();
+        }
+
+        public override void SetLayoutHorizontal() {
+            throw new System.NotImplementedException();
+        }
+
+        public override void SetLayoutVertical() {
+            throw new System.NotImplementedException();
+        }
+    }
+}
