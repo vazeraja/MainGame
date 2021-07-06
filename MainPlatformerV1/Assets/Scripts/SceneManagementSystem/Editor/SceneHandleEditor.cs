62a81b886d91aed5dda785a46fd45882a9d10165
vazeraja <68131084+vazeraja@users.noreply.github.com>
2021-07-05 22:55:14 -0400

    scene hander tool beginnings

---
+++ MainPlatformerV1/Assets/Scripts/SceneManagementSystem/Editor/SceneHandleEditor.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+using UnityEditor;
+using UnityEngine;
+
+namespace ThunderNut.SceneManagement.Editor {
+    
+    [CustomEditor(typeof(SceneHandle))]
+    public class SceneHandleEditor : UnityEditor.Editor {
+        public override void OnInspectorGUI() {
+            base.OnInspectorGUI();
+        }
+        
+        
+
+
+        [MenuItem("Assets/Create/World Graph/Scene Handle (From Scene)", false, 400)]
+        private static void CreateFromTextures()
+        {
+            var trailingNumbersRegex = new Regex(@"(\d+$)");
+
+            var scene = Selection.activeObject as SceneAsset;
+
+            var asset = CreateInstance<SceneHandle>();
+            asset.scene = scene;
+            string baseName = trailingNumbersRegex.Replace(scene != null ? scene.name : string.Empty, "");
+            asset.name = baseName + "Handle";
+
+            string assetPath = Path.GetDirectoryName(AssetDatabase.GetAssetPath(scene));
+            AssetDatabase.CreateAsset(asset, Path.Combine(assetPath ?? Application.dataPath, asset.name + ".asset"));
+            AssetDatabase.SaveAssets();
+        }
+
+        [MenuItem("Assets/Create/World Graph/Scene Handle (From Scene)", true, 400)]
+        private static bool CreateFromTexturesValidation() => Selection.activeObject as SceneAsset;
+    }
+}
---
+++ MainPlatformerV1/Assets/Scripts/SceneManagementSystem/Runtime/SceneHandle.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.LowLevel;
+using UnityEngine.SceneManagement;
+
+
+[CreateAssetMenu(fileName = "SceneHandle", menuName = "World Graph/Scene Handle")]
+public class SceneHandle : ScriptableObject {
+
+    public SceneAsset scene;
+    
+
+}
