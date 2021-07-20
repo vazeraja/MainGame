382349f9bc4f9f1d90a421a61415313208e5651a
vazeraja <68131084+vazeraja@users.noreply.github.com>
2021-07-20 15:44:48 -0400

    --

--- MainPlatformerV1/Assets/Scripts/Input/InputProvider.cs
+++ MainPlatformerV1/Assets/Scripts/Input/InputProvider.cs
@@ -6,14 +6,15 @@ public struct InputState {
     public Vector2 movementDirection;
     public bool isCrouching;
 }
+
 public interface IInputProvider {
     //public event UnityAction jump;
     public InputState GetState();
 }
 
 [CreateAssetMenu(fileName = "InputReader", menuName = "InputData/Input Reader")]
-public class InputProvider : ScriptableObject, IInputProvider, GameInput.IGameplayActions, GameInput.IDialoguesActions, GameInput.IMenuActions{
-    
+public class InputProvider : ScriptableObject, IInputProvider, GameInput.IGameplayActions, GameInput.IDialoguesActions,
+    GameInput.IMenuActions {
     // Player
     public event UnityAction<Vector2> MoveEvent;
     public event UnityAction<float> JumpEvent;
@@ -24,24 +25,28 @@ public class InputProvider : ScriptableObject, IInputProvider, GameInput.IGamepl
     private bool isCrouching;
 
     #region Other
+
     public event UnityAction InteractionStartedEvent;
     public event UnityAction InteractionCancelledEvent;
     public event UnityAction HatEvent;
     public event UnityAction OpenDevConsole;
     public event UnityAction AdvanceDialogueEvent;
+
     #endregion
-    
+
     #region Menu Events
+
     public event UnityAction OpenMenuWindow;
     public event UnityAction TabRightButtonEvent;
     public event UnityAction TabLeftButtonEvent;
     public event UnityAction CloseMenuWindow;
 
     #endregion
-    
+
     private GameInput GameInput { get; set; }
 
-    private void OnEnable() {
+    private void OnEnable()
+    {
         if (GameInput == null) {
             GameInput = new GameInput();
             GameInput.Gameplay.SetCallbacks(this);
@@ -56,38 +61,48 @@ public class InputProvider : ScriptableObject, IInputProvider, GameInput.IGamepl
 
     #region Gameplay Actions
 
-    public void OnMove(InputAction.CallbackContext context) {
+    public void OnMove(InputAction.CallbackContext context)
+    {
         MoveEvent?.Invoke(context.ReadValue<Vector2>().normalized);
         movementDirection = context.ReadValue<Vector2>();
     }
-    
-    public void OnCrouch(InputAction.CallbackContext context) {
+
+    public void OnCrouch(InputAction.CallbackContext context)
+    {
         if (CrouchEvent != null && context.phase == InputActionPhase.Performed) {
             CrouchEvent?.Invoke(context.ReadValue<float>());
+            isCrouching = context.ReadValue<float>() > 0.5f;
         }
     }
-    public void OnJump(InputAction.CallbackContext context) {
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
         if (JumpEvent != null && context.phase == InputActionPhase.Performed) {
             JumpEvent?.Invoke(context.ReadValue<float>());
         }
     }
-    public void OnDash(InputAction.CallbackContext context) {
+
+    public void OnDash(InputAction.CallbackContext context)
+    {
         if (DashEvent != null && context.phase == InputActionPhase.Performed)
             DashEvent.Invoke(context.ReadValue<float>());
     }
 
-    public void OnHat(InputAction.CallbackContext context) {
+    public void OnHat(InputAction.CallbackContext context)
+    {
         if (HatEvent != null && context.phase == InputActionPhase.Started)
             HatEvent?.Invoke();
     }
 
 
-    public void OnOpenDevConsole(InputAction.CallbackContext context) {
+    public void OnOpenDevConsole(InputAction.CallbackContext context)
+    {
         if (OpenDevConsole != null && context.phase == InputActionPhase.Performed)
             OpenDevConsole.Invoke();
     }
 
-    public void OnInteract(InputAction.CallbackContext context) {
+    public void OnInteract(InputAction.CallbackContext context)
+    {
         if (InteractionStartedEvent != null && context.phase == InputActionPhase.Started)
             InteractionStartedEvent.Invoke();
 
@@ -95,7 +110,8 @@ public class InputProvider : ScriptableObject, IInputProvider, GameInput.IGamepl
             InteractionCancelledEvent.Invoke();
     }
 
-    public void OnMenu(InputAction.CallbackContext context) {
+    public void OnMenu(InputAction.CallbackContext context)
+    {
         if (OpenMenuWindow != null && context.phase == InputActionPhase.Started)
             OpenMenuWindow?.Invoke();
     }
@@ -104,7 +120,8 @@ public class InputProvider : ScriptableObject, IInputProvider, GameInput.IGamepl
 
     #region Dialogue Actions
 
-    public void OnAdvanceDialogue(InputAction.CallbackContext context) {
+    public void OnAdvanceDialogue(InputAction.CallbackContext context)
+    {
         if (context.phase == InputActionPhase.Performed)
             AdvanceDialogueEvent?.Invoke();
     }
@@ -113,50 +130,67 @@ public class InputProvider : ScriptableObject, IInputProvider, GameInput.IGamepl
 
     #region Menu Actions
 
-    public void OnTabLeft(InputAction.CallbackContext context) {
+    public void OnTabLeft(InputAction.CallbackContext context)
+    {
         if (TabLeftButtonEvent != null && context.phase == InputActionPhase.Started) {
             TabLeftButtonEvent?.Invoke();
         }
     }
 
-    public void OnTabRight(InputAction.CallbackContext context) {
+    public void OnTabRight(InputAction.CallbackContext context)
+    {
         if (TabRightButtonEvent != null && context.phase == InputActionPhase.Started)
             TabRightButtonEvent?.Invoke();
     }
 
-    public void OnCloseMenu(InputAction.CallbackContext context) {
+    public void OnCloseMenu(InputAction.CallbackContext context)
+    {
         if (CloseMenuWindow != null && context.phase == InputActionPhase.Started)
             CloseMenuWindow?.Invoke();
     }
 
     #endregion
 
-    public void EnableGameplayInput() {
+    #region Helpful Methods
+    public void EnableGameplayInput()
+    {
         GameInput.Gameplay.Enable();
         GameInput.Dialogues.Disable();
         GameInput.Menu.Disable();
     }
 
-    public void EnableDialogueInput() {
+    public void EnableDialogueInput()
+    {
         GameInput.Dialogues.Enable();
         GameInput.Gameplay.Disable();
         GameInput.Menu.Disable();
     }
 
-    public void EnableMenuInput() {
+    public void EnableMenuInput()
+    {
         GameInput.Menu.Enable();
         GameInput.Gameplay.Disable();
         GameInput.Dialogues.Disable();
     }
 
-    public void DisableAllInput() {
+    public void DisableAllInput()
+    {
         GameInput.Gameplay.Disable();
         GameInput.Dialogues.Disable();
         GameInput.Menu.Disable();
     }
-    public InputState GetState() {
+    #endregion
+
+    public InputState GetState()
+    {
         return new InputState {
-            movementDirection = movementDirection
+            movementDirection = movementDirection,
+            isCrouching = isCrouching,
         };
+    }
+
+    public static implicit operator InputState(InputProvider provider)
+    {
+        return provider.GetState();
     }
 }
--- MainPlatformerV1/Assets/Scripts/Reanimator+SM/Sasuke/PlayerController.cs
+++ MainPlatformerV1/Assets/Scripts/Reanimator+SM/Sasuke/PlayerController.cs
@@ -12,30 +12,24 @@ public enum SasukeState {
 }
 
 public class PlayerController : MonoBehaviour, ISaveable {
-    
-    [Header("Input")] 
-    public InputProvider inputProvider;
-    
-    [Header("Walking")] 
-    public float walkSpeed = 7;
+    [Header("Input")] public InputProvider inputProvider;
 
-    [Header("Jumping")] 
-    public float firstJumpSpeed;
+    [Header("Walking")] public float walkSpeed = 7;
+
+    [Header("Jumping")] public float firstJumpSpeed;
     public float secondJumpSpeed;
     public float fallSpeed;
     public int numberOfJumps = 2;
     public AnimationCurve jumpFallOff = AnimationCurve.Linear(0, 1, 1, 0);
     public FixedStopwatch jumpStopwatch = new FixedStopwatch();
 
-    [Header("Getting a whooping")] 
-    public Vector2 hitForce;
+    [Header("Getting a whooping")] public Vector2 hitForce;
     public FixedStopwatch hitStopwatch = new FixedStopwatch();
     public Collision2D collisionData;
 
-    [Header("Giving a whooping")] 
-    public float dashSpeed = 12;
+    [Header("Giving a whooping")] public float dashSpeed = 12;
     public FixedStopwatch dashStopwatch = new FixedStopwatch();
-    
+
     private RuntimeStateMachine stateMachine;
     public CollisionDetection CollisionDetection { get; set; }
     public Vector2 MovementDirection { get; set; }
@@ -55,63 +49,75 @@ public class PlayerController : MonoBehaviour, ISaveable {
     [HideInInspector] public bool canDash;
     [HideInInspector] public int enemyLayer;
 
-    private void Awake() {
+    private void Awake()
+    {
         CollisionDetection = GetComponent<CollisionDetection>();
 
-        RuntimeStateMachine stateMachine = Builder.RuntimeStateMachine
+        stateMachine = Builder.RuntimeStateMachine
             .WithState(new MovementState(), out var movementState)
             .WithState(new DashState(), out var dashState)
             .WithState(new HitState(), out var hitState)
             .WithState(new RemainState(), out var remainState)
             .WithTransition(new Transition(), new EnterMovementStateDecision(), movementState, remainState,
                 new[] {dashState, hitState})
-            .WithTransition(new Transition(), new EnterDashStateDecision(), dashState, remainState, 
+            .WithTransition(new Transition(), new EnterDashStateDecision(), dashState, remainState,
                 new[] {movementState})
             .WithTransition(new Transition(), new EnterHitStateDecision(), hitState, remainState,
                 new[] {movementState, dashState})
             .SetCurrentState(movementState)
             .SetRemainState(remainState);
     }
-    private void OnEnable() {
+
+    private void OnEnable()
+    {
         inputProvider.MoveEvent += OnMove;
         inputProvider.JumpEvent += OnJump;
         inputProvider.DashEvent += OnDash;
         inputProvider.CrouchEvent += OnCrouch;
     }
 
-    private void OnDisable() {
+    private void OnDisable()
+    {
         inputProvider.MoveEvent -= OnMove;
         inputProvider.JumpEvent -= OnJump;
         inputProvider.CrouchEvent -= OnCrouch;
         inputProvider.DashEvent -= OnDash;
     }
-    
-    private void Start() {
+
+    private void Start()
+    {
         JsonSaveService.LoadJsonData(new[] {this});
         enemyLayer = LayerMask.NameToLayer($"Enemy");
         stateMachine.Bind<PlayerController>(this);
+
     }
 
-    private void Update() {
-        Debug.Log(inputProvider.GetState().movementDirection);
+    private InputState inputState;
+    private void Update()
+    {
+        inputState = inputProvider;
         stateMachine.Update();
-    } 
+        Debug.Log("Movement Direction: " + inputState.movementDirection + "\n IsCrouching: " + inputState.isCrouching);
+    }
 
     private void FixedUpdate() => stateMachine.FixedUpdate();
 
-    private void OnApplicationQuit() {
-        JsonSaveService.SaveJsonData(new []{this});
+    private void OnApplicationQuit()
+    {
+        JsonSaveService.SaveJsonData(new[] {this});
     }
 
     #region Events
 
     private void OnMove(Vector2 value) => MovementDirection = value;
 
-    private void OnJump(float value) {
+    private void OnJump(float value)
+    {
         wantsToJump = value > 0.5f;
         HandleJump();
 
-        void HandleJump() {
+        void HandleJump()
+        {
             if (wantsToJump) {
                 if (State != SasukeState.Movement || jumpsLeft <= 0)
                     return;
@@ -127,19 +133,22 @@ public class PlayerController : MonoBehaviour, ISaveable {
 
     private void OnDash(float value) => EnterDashState();
 
-    private void OnCrouch(float value) {
+    private void OnCrouch(float value)
+    {
         IsCrouching = value > 0.5f;
     }
 
     #endregion
 
-    private void OnCollisionEnter2D(Collision2D other) {
+    private void OnCollisionEnter2D(Collision2D other)
+    {
         if (other.gameObject.layer != enemyLayer) return;
         collisionData = other;
         EnterHitState();
     }
 
-    private void OnCollisionStay2D(Collision2D other) {
+    private void OnCollisionStay2D(Collision2D other)
+    {
         if (other.gameObject.layer != enemyLayer || State == SasukeState.Hit) return;
         collisionData = other;
         EnterHitState();
@@ -147,28 +156,32 @@ public class PlayerController : MonoBehaviour, ISaveable {
 
     #region States
 
-    private void EnterHitState() {
+    private void EnterHitState()
+    {
         if (State != SasukeState.Hit && !hitStopwatch.IsReady) return;
         State = SasukeState.Hit;
     }
 
-    private void EnterDashState() {
+    private void EnterDashState()
+    {
         if (State != SasukeState.Movement || !dashStopwatch.IsReady || !canDash) return;
         State = SasukeState.Dash;
-        
     }
 
-    public void EnterMovementState() {
+    public void EnterMovementState()
+    {
         State = SasukeState.Movement;
     }
 
     #endregion
 
-    public void PopulateSaveData(SaveData saveData) {
+    public void PopulateSaveData(SaveData saveData)
+    {
         saveData.profile.location = transform.position;
     }
 
-    public void LoadFromSaveData(SaveData saveData) {
+    public void LoadFromSaveData(SaveData saveData)
+    {
         transform.position = saveData.profile.location;
     }
 }
